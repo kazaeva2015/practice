50ab98a49b369757cbb02a38abb9b7f4344d0da3
Анна Казаева <76570998+kazaeva2015@users.noreply.github.com>
2023-05-14 20:18:55 +0400

    Delete modules directory

--- modules/system/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-
-@admin.register(Profile)
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'birth_date', 'slug')
-    list_display_links = ('user', 'slug')
